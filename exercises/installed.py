7474153afafb9e18edb3f609eb94c3a79f979c6c
Frank Cass <frankcass1@gmail.com>
2017-05-04 18:19:40 -0400

    Initial Commit

---
+++ exercises/installed.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Provides a list of locally installed python modules and their version
+
+import pip
+installed_packages = pip.get_installed_distributions()
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+     for i in installed_packages])
+print(installed_packages_list)
+
