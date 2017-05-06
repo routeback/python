8fac6744e63418fe89dbc6dbb0a5adb4dae0dd00
Frank Cass <frankcass1@gmail.com>
2017-05-06 17:09:36 -0400

    Initial Commit

---
+++ exercises/randomizer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Randomizer - Picks a number one through five, with one being the lucky number.
+
+import random
+
+oneinfive = (1, 2, 3, 4, 5)
+
+if (random.choice(oneinfive)) == 1:
+	print("Success! Your number was selected")
+elif (random.choice(task)) == 2 or 3 or 4 or 5:
+	print("Fail! Your number was not selected.")
