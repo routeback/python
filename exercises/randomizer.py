94029f94f81fd661d7f90745931698854683e7da
Frank Cass <frankcass1@gmail.com>
2017-05-06 17:28:10 -0400

    Added a period

--- exercises/randomizer.py
+++ exercises/randomizer.py
@@ -7,6 +7,6 @@ import random
 oneinfive = (1, 2, 3, 4, 5)
 
 if (random.choice(oneinfive)) == 1:
-	print("Success! Your number was selected")
+	print("Success! Your number was selected.")
 elif (random.choice(oneinfive)) == 2 or 3 or 4 or 5:
 	print("Fail! Your number was not selected.")
